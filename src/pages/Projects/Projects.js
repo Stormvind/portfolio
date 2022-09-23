167a6fbaae9598d313c860b98a204bc61850f976
Stormvind <voddy@protonmail.com>
2022-09-23 07:35:28 +0200

    Fixed CSS naming collisions and an overflow problem on very small monitors just barely bigger than the phone media query target

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -8,25 +8,25 @@ function Projects() {
 	return (
 		<>
 			<Navcontainer />
-				<main className="main-content" id="main-content">
+				<main className="project-main-content" id="main-content">
 					{Projectsdata.projects.map((project, index)=>
-						<div className="main-contet__entry-wrapper" key={index}>
+						<div className="project-main-content__entry-wrapper" key={index}>
 							<img 
-								className="main-content__img"
+								className="project-main-content__img"
 								src={project.imgPath}
 								alt="screenshot"
 							/>
-							<div className="main-content__project-text">
+							<div className="project-main-content__project-text">
 								<div>
-									<h1 className="main-content__name">{project.name}</h1>
-									<h2 className="main-content__date">{project.date}</h2>
-									<p className="main-content__description">{project.description}</p>
+									<h1 className="project-main-content__name">{project.name}</h1>
+									<h2 className="project-main-content__date">{project.date}</h2>
+									<p className="project-main-content__description">{project.description}</p>
 								</div>
 								<div>
-									<p className="main-content__link">
+									<p className="project-main-content__link">
 										<a href={project.sourceCodeLink}>Source code on GitHub</a>
 									</p>
-									<p className="main-content__link">
+									<p className="project-main-content__link">
 										<a href={project.deployedAppLink}>Deployed app</a>
 									</p>
 								</div>
